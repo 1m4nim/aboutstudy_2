c83e6f83e313b5af47ac175b981be9d084973d07
south <fancustard9@gmail.com>
2024-10-16 12:10:56 +0900

    styleが重複してた

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -144,16 +144,16 @@ export default function Todo() {
                         <Content>
                             <Form onFinish={handleGoalSubmit} layout="vertical">
                                 <Form.Item label="やる予定のもの" required>
-                                    <Input style={{ fontsize: "16px" }} value={goalInput} onChange={(e) => setGoalInput(e.target.value)} />
+                                    <Input style={{ fontSize: "16px" }} value={goalInput} onChange={(e) => setGoalInput(e.target.value)} />
                                 </Form.Item>
                                 <Form.Item label="日付" required>
-                                    <DatePicker style={{ fontsize: "16px" }} value={goalDate} onChange={setGoalDate} style={{ width: '100%' }} />
+                                    <DatePicker style={{ width: "100%", fontSize: "16px" }} value={goalDate} onChange={setGoalDate} />
                                 </Form.Item>
                                 <Form.Item label="開始時間" required>
-                                    <TimePicker style={{ fontsize: "16px" }} value={goalStartTime} onChange={setGoalStartTime} format="HH:mm" style={{ width: '100%' }} />
+                                    <TimePicker style={{ width: "100%", fontSize: "16px" }} value={goalStartTime} onChange={setGoalStartTime} format="HH:mm" />
                                 </Form.Item>
                                 <Form.Item label="終了時間" required>
-                                    <TimePicker style={{ fontsize: "16px" }} value={goalEndTime} onChange={setGoalEndTime} format="HH:mm" style={{ width: '100%' }} />
+                                    <TimePicker style={{ width: "100%", fontSize: "16px" }} value={goalEndTime} onChange={setGoalEndTime} format="HH:mm" />
                                 </Form.Item>
                                 <Button type="primary" htmlType="submit" block >
                                     送信
@@ -177,16 +177,16 @@ export default function Todo() {
                         <Content>
                             <Form onFinish={handleDoneSubmit} layout="vertical">
                                 <Form.Item label="やったこと" required>
-                                    <Input style={{ fontsize: "16px" }} value={doneInput} onChange={(e) => setDoneInput(e.target.value)} />
+                                    <Input style={{ fontSize: "16px" }} value={doneInput} onChange={(e) => setDoneInput(e.target.value)} />
                                 </Form.Item>
                                 <Form.Item label="日付" required>
-                                    <DatePicker style={{ fontsize: "16px" }} value={doneDate} onChange={setDoneDate} style={{ width: '100%' }} />
+                                    <DatePicker style={{ width: "100%", fontSize: "16px" }} value={doneDate} onChange={setDoneDate} />
                                 </Form.Item>
                                 <Form.Item label="やり始めた時間" required>
-                                    <TimePicker style={{ fontsize: "16px" }} value={doneStartTime} onChange={setDoneStartTime} format="HH:mm" style={{ width: '100%' }} />
+                                    <TimePicker style={{ width: "100%", fontSize: "16px" }} value={doneStartTime} onChange={setDoneStartTime} format="HH:mm" />
                                 </Form.Item>
                                 <Form.Item label="終了時間" required>
-                                    <TimePicker style={{ fontsize: "16px" }} value={doneEndTime} onChange={setDoneEndTime} format="HH:mm" style={{ width: '100%' }} />
+                                    <TimePicker style={{ width: "100%", fontSize: "16px" }} value={doneEndTime} onChange={setDoneEndTime} format="HH:mm" />
                                 </Form.Item>
                                 <Button type="primary" htmlType="submit" block>
                                     送信
